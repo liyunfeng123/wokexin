71685e1a94621f2e9c8c498f4922464a10899411
liyunfeng123 <64241576+liyunfeng123@users.noreply.github.com>
2020-08-27 18:15:44 +0800

    Update ddt_csv.py

--- ddt_csv.py
+++ ddt_csv.py
@@ -1,60 +1,87 @@
-from ddt import ddt,data,unpack
+# coding=utf-8
+# !/usr/bin/python
+import csv
+import codecs
 from selenium import webdriver
+from time import sleep
+from selenium.webdriver import ActionChains
 import unittest
-import time
-from HTMLTestRunner import HTMLTestRunner
-# def get_data(file_name):
-#      # 读取本地 CSV 文件
-#      rows=[]
-#      data_file =open(file_name,'r')
-#      reader=csv.reader(data_file)
-#      for user in reader:
-#          rows.append(user)
-#      return rows
-def get_data():
-   # 写方法传入数据
-   return [
-          ["16800000015","123456"],
-          ["16800000018","123456"]
-          ]
-@ddt
-class YunXunCase(unittest.TestCase):
-        def setUp(self):
-                self.driver = webdriver.Chrome()
-                self.driver.maximize_window()
-                self.driver.get("https://test.yunwoke.com/")
-        @data(*get_data())
 
-        # @data(*get_data('haoma.csv'))
-        # @data(*get_data('zhanghao.txt'))
-        # @data(('16800000015','123456'),('16800000018','123456'))
-        @unpack
-        def test_case1(self, phone,pwd):
-
-                driver = self.driver
-                driver.find_element_by_xpath('//*[@id="homePage"]/nav/section/div[1]/div/div/ul[3]/li[1]/a').click()
-                driver.find_element_by_xpath("//input[@placeholder='手机号／邮箱']").send_keys(phone)
-                driver.find_element_by_xpath("//input[@placeholder='密码']").send_keys(pwd)
-                time.sleep(2)
-                driver.find_element_by_xpath('//*[@id="app"]/section/article[2]/div/div[2]/div/div[1]/section/button[1]').click()
-                time.sleep(5)
-                s =driver.find_element_by_xpath('//*[@id="published"]/nav/section/div[2]/ul/li[1]/a').text
-                print(s)
-                self.assertEqual(s,'首页','失败')
-                print("测试成功，结果与预期匹配")
+options = webdriver.ChromeOptions()
+prefs = {
+    'profile.default_content_setting_values':
+        {
+            'notifications': 2
+        }
+}
+options.add_experimental_option('prefs', prefs)  # 关掉浏览器左上角的通知提示，如上图
+options.add_argument('disable-infobars')  # 关闭'chrome正受到自动测试软件的控制'提示
 
 
-        def tearDown(self):
-                self.driver.quit()
+class Test_Demo(unittest.TestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(chrome_options=options)
+        cls.driver.get("https://ent.yunwoke.com/app/message")
+        cls.driver.maximize_window()
+        cls.driver.implicitly_wait(10)
+        cls.driver.find_element_by_xpath("//input[@placeholder='手机号／邮箱']").send_keys("16800000013")
+        cls.driver.find_element_by_xpath("//input[@placeholder='密码']").send_keys("123456")
+        cls.driver.find_element_by_xpath('//button[@class="next-btn"][1]').click()
+        # 等待
+        cls.driver.implicitly_wait(18)
+        cls.driver.find_element_by_xpath("//p[text()='跳过']").click()
+        cls.driver.implicitly_wait(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        # cls.driver.close()
+        pass
+
+
+    def test_addhaoyou(self):
+        driver = self.driver
+
+        driver.find_element_by_xpath('//*[@id="companyHeader"]/div/div/div[3]/div[3]/img').click()
+        sleep(1)
+        driver.find_element_by_xpath('/html/body/div[4]/div/div/ul/a[4]/li').click()
+
+        wins = driver.window_handles
+        driver.switch_to.window(wins[1])
+        driver.find_element_by_xpath('//*[@id="root"]/section/aside/div/div[2]/ul/li[2]/div[1]/span').click()
+        sleep(1)
+        driver.find_element_by_xpath('//*[@id="/admin/org$Menu"]/li[1]/a').click()
+        driver.implicitly_wait(8)
+        # 读取本地 CSV 文件
+        datas = csv.reader(codecs.open('phone.csv', 'r', 'utf_8_sig'))
+
+        for user in datas:
+            # 添加成员
+            driver.find_element_by_css_selector('#root > section > section > main > div > main > div > div.main > div > div.list-hd > div.action > button:nth-child(1)').click()
+            # driver.find_element_by_xpath('//*[@id="root"]/section/section/main/div/main/div/div[2]/div/div[1]/div[2]/button[1]/span').click()
+            # 输入手机号
+            driver.find_element_by_xpath('//*[@id="memberPhone"]').send_keys(user[1])
+            # 输入姓名
+            driver.find_element_by_xpath('//*[@id="memberName"]').send_keys(user[0])
+            # 员工类型
+            driver.find_element_by_xpath('//*[@id="memberType"]/div/div').click()
+            driver.find_element_by_xpath("//ul[@class='ant-select-dropdown-menu  ant-select-dropdown-menu-root ant-select-dropdown-menu-vertical']/li[1]").click()
+            driver.get_screenshot_as_file('E:\\zidonghua\\haoma.png')
+            try:
+                # 保存
+                driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button[3]').click()
+
+            finally:
+                # c = driver.find_element_by_xpath('/html/body/div[5]/div/span/div/div/div/span').text
+                # print(c)
+                driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button[1]').click()
+            driver.implicitly_wait(5)
+
+
+
 
 
 if __name__ == '__main__':
     unittest.main()
-    # testsuite = unittest.TestSuite()
-    # testsuite.addTest(YunXunCase("test_case1"))
-    #
-    # rst_path = "E:\\All_Test\\Test_Result\\test_case1.html"
-    # fp = open(rst_path, 'wb')
-    #
-    # runner = HTMLTestRunner(title='test', description='云讯自动化用例', stream=fp, verbosity=2, retry=0, save_last_try=True)
-    # runner.run(testsuite)
